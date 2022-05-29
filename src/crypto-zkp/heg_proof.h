ab888a355b7efa0ea30fb4f17c3165fbc7ece4b0
何剑虹 <hejh1500@gmail.com>
2022-05-29 13:23:57 +0800

    Update example; update comments;

--- src/crypto-zkp/heg_proof.h
+++ src/crypto-zkp/heg_proof.h
@@ -1,6 +1,12 @@
-//
-// Created by Sword03 on 2021/6/15.
-//
+/*
+ * Copyright 2020-2022 Safeheron Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.safeheron.com/opensource/license.html
+ */
+
 
 #ifndef SAFEHERON_CRYPTO_ZKP_HEG_PROOF_H
 #define SAFEHERON_CRYPTO_ZKP_HEG_PROOF_H
